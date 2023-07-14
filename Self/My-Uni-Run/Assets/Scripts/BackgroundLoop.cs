72241702bfe9f04dc656c9fc4e8200c1b9f8ad24
N3wCP <61874428+Toyoill@users.noreply.github.com>
2023-07-14 20:50:55 +0900

    My Uni-Run

---
+++ Self/My-Uni-Run/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
+public class BackgroundLoop : MonoBehaviour {
+    private float width; // 배경의 가로 길이
+
+    private void Awake() {
+        // 가로 길이를 측정하는 처리
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        width = boxCollider.size.x;
+    }
+
+    private void Update() {
+        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+        if (transform.position.x < -width) {
+            Reposition();
+        }
+    }
+
+    // 위치를 리셋하는 메서드
+    private void Reposition() {
+        transform.Translate(Vector2.right * width * 2f);
+    }
+}
